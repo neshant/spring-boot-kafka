658e18f42db4288a6562ff2e2c78078c31d718c1
neshant <neshantsharma89@gmail.com>
2019-05-01 20:48:44 -0700

    added the files from spring initialiser and added some more dependencies

---
+++ src/test/kotlin/com/neshant/sprintbootkafka/SprintBootKafkaApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.neshant.sprintbootkafka
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SprintBootKafkaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
